5de64d1bd296aa6f4c3dbbbb60007faaca8c75e8
Jarryd Huntley <Jarryd Huntley>
2017-11-13 13:31:54 -0500

    initial commit.

---
+++ Chatter_Fall17/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chatter_Fall17.Startup))]
+namespace Chatter_Fall17
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
